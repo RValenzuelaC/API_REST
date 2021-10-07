097990a470973c586ff9548c699f1c6bc40f2e8a
Raúl Valenzuela <raul.valenzuela@alumnos.uv.cl>
2021-10-07 11:17:15 -0300

    API_REST in php

---
+++ generate_hash.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$time = time();
+echo "Time: $time".PHP_EOL."Hash: ".sha1($argv[1].$time.'Secreto');
---
+++ server.php
@@ -0,0 +1,142 @@
+<?php
+
+
+/*if (
+    !array_key_exists('HTTP_X_HASH', $_SERVER) ||
+    !array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) ||
+    !array_key_exists('HTTP_X_UID', $_SERVER)
+) {
+
+    die;
+}
+
+list($hash, $uid, $timestamp) = [
+    $_SERVER['HTTP_X_HASH'],
+    $_SERVER['HTTP_X_UID'],
+    $_SERVER['HTTP_X_TIMESTAMP']
+];
+
+
+$secret = 'Secreto';
+
+$newHash = sha1($uid . $timestamp . $secret);
+
+if ($newHash !== $hash) {
+    die;
+}*/
+
+if (!array_key_exists('HTTP_X_TOKEN', $_SERVER)) {
+
+    die;
+}
+
+$url = 'https://' . $_SERVER['HTTP_HOST'] . '/auth';
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "X-Token: {$_SERVER['HTTP_X_TOKEN']}",
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$ret = curl_exec($ch);
+
+if (curl_errno($ch) != 0) {
+    die(curl_error($ch));
+}
+
+if ($ret !== 'true') {
+    http_response_code(403);
+
+    die;
+}
+
+
+
+// Definimos los recursos disponibles
+$allowedResourceTypes = [
+
+    'books',
+    'authors',
+    'genres',
+
+
+];
+//Validamos que el recurso este disponible
+$resorceType = $_GET['resource_type'];
+
+if (!in_array($resorceType, $allowedResourceTypes)) {
+    http_response_code(400);
+    die;
+}
+//Defino los recursos
+$books = [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    2 => [
+        'titulo' => 'La Iliada',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+];
+header('Content-Type: application/json');
+
+//Levantamos el id del recurso buscado
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
+
+//Generamos la respuesta asumiendo que el pedido es correcto
+switch (strtoupper($_SERVER['REQUEST_METHOD'])) {
+    case 'GET':
+        if (empty($resourceId)) {
+            echo json_encode($books);
+        } else {
+            if (array_key_exists($resourceId, $books)) {
+                echo    json_encode($books[$resourceId]);
+            } else {
+                http_response_code(404);
+            }
+        }
+
+        break;
+    case 'POST':
+        $json = file_get_contents('php://input');
+
+        //Transformamos el json recibido a un nuevo elemento del arreglo
+        $books[] = json_decode($json, true);
+
+        //echo array_keys($books)[count($books) - 1];
+        echo json_encode($books);
+
+        break;
+    case 'PUT':
+        //Validamos que el recurso buscado exista
+        if (!empty($resourceId) && array_key_exists($resourceId, $books)) {
+            //Tomamos la entrada
+
+            $json = file_get_contents('php://input');
+
+            //Transformamos el json recibido a un nuevo elemento del arreglo
+            $books[$resourceId] = json_decode($json, true);
+
+            //Tomamos la coleccion modificada en formato json
+            echo json_encode($books);
+        }
+
+        break;
+    case 'DELETE':
+        //Validamos que el recurso exista
+        if (!empty($resourceId) && array_key_exists($resourceId, $books)) {
+
+            //Eliminamos el recurso
+            unset($books[$resourceId]);
+        }
+        echo json_encode($books);
+
+        break;
+}
